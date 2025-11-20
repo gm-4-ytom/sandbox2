40d3225ac31902282f0b8936bf027c81f34e3663
gm.4.ytom <gm.4.ytom@gmail.com>
2025-11-20 22:54:49 +0900

    add service

--- quarkusdemo/src/main/java/jp/co/example/GreetingResource.java
+++ quarkusdemo/src/main/java/jp/co/example/GreetingResource.java
@@ -1,16 +1,25 @@
 package jp.co.example;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jp.co.example.service.GreetingService;
 
 @Path("/hello")
 public class GreetingResource {
 
+    GreetingService service;
+
+    @Inject
+    GreetingResource(GreetingService service) {
+        this.service = service;
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return service.getMessage();
     }
 }
